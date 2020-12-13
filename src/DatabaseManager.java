166c81301453b1a8d68800d8990d21d6985cd6ba
iNYU_ <inyu621@gmail.com>
2020-12-13 20:59:02 +0900

    get ready - 1

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.sql.*;
 
-/**
- *
- * @author wkdgu
- */
 public class DatabaseManager {
 
     String url = "localhost:3306";
